dd4c5c505cf0ee901beff72f20ffcf1a878a6bf7
João Guilherme <joao.guilherme@alternativa.infra>
2025-02-06 13:36:10 -0300

    atualizacao

--- script.js
+++ script.js
@@ -14,14 +14,62 @@ function calcular() {
     document.getElementById('resultado').innerText = `Você gastará aproximadamente R$ ${custoTotal.toFixed(2)}.`;
 }
 
+function calcularCombustivel() {
+    let precoGasolina = parseFloat(document.getElementById("gasolina").value);
+    let precoEtanol = parseFloat(document.getElementById("etanol").value);
+
+    if (isNaN(precoGasolina) || isNaN(precoEtanol)) {
+        document.getElementById("resultado").innerText = "Por favor, preencha ambos os campos!";
+        return;
+    }
+
+    let limiteEtanol = precoGasolina * 0.7;
+
+    if (precoEtanol <= limiteEtanol) {
+        document.getElementById("resultado").innerText = "Vale a pena abastecer com Etanol!";
+    } else {
+        document.getElementById("resultado").innerText = "Vale a pena abastecer com Gasolina!";
+    }
+}
+
+function atualizarTráfego() {
+    var trafego = document.getElementById("trafego").value;
+    var trafegoTexto = document.getElementById("trafegoTexto");
+
+    if (trafego == "0") {
+        trafegoTexto.innerHTML = "Trânsito Leve (75Km/H)";
+        trafegoTexto.style.color = "white"; 
+        trafegoTexto.style.fontSize = "16px"; 
+    } else if (trafego == "1") {
+        trafegoTexto.innerHTML = "Trânsito Moderado (50Km/H)";
+        trafegoTexto.style.color = "white"; 
+        trafegoTexto.style.fontSize = "16px"; 
+    } else if (trafego == "2") {
+        trafegoTexto.innerHTML = "Trânsito Pesado (30Km/H)";
+        trafegoTexto.style.color = "white"; 
+        trafegoTexto.style.fontSize = "16px"; 
+    }
+}
+
+function calcularTempo() {
+    var distancia = document.getElementById("distancia").value;
+    var trafego = document.getElementById("trafego").value;
 
 
+    var velocidades = [75, 50, 30];
 
+    if (distancia && trafego !== "") {
+        var velocidadeFinal = velocidades[trafego];
 
+        var tempo = distancia / velocidadeFinal;
+        var horas = Math.floor(tempo);
+        var minutos = Math.round((tempo - horas) * 60);
 
-
-
-
+        document.getElementById("resultado").innerHTML = `Tempo estimado de chegada: ${horas} horas e ${minutos} minutos (Com ${document.getElementById("trafegoTexto").innerText})`;
+    } else {
+        document.getElementById("resultado").innerHTML = "Por favor, preencha todos os campos corretamente.";
+    }
+}
 
 
 
